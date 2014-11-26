5b7bd4e47a7119f30b567f13bc1749a73be63148
Victor Nystad <vnystad@gmail.com>
2014-11-26 21:45:40 +0100

    Update 2014-11-26T20:45:40.150Z

--- script/script.js
+++ script/script.js
@@ -1,9 +1,13 @@
-var menu = document.querySelector('nav'), header = document.querySelector('header');
+var menu = document.querySelector('nav'), header = document.querySelector('header'), wrapper = document.getElementById('wrapper');
 
 var hugeMin = window.matchMedia('(min-width: 1280px)');
+var smallMax = window.matchMedia('(max-width: 767px)');
 
 hugeMin.addListener(isHuge);
 isHuge(hugeMin);
+
+smallMax.addListener(isSmall);
+isSmall(smallMax);
 
 function isHuge(mq) {
     if (hugeMin.matches) {
@@ -14,5 +18,15 @@ function isHuge(mq) {
         header.parentNode.insertBefore(menu, header.nextSibling);
         console.log('less than huge');
       /* the view port is less than 400 pixels wide */
+    }
+}
+
+function isSmall(mq) {
+    if (smallMax.matches) {
+        console.log('small or less');
+        document.body.insertBefore(menu, wrapper.nextSibling);
+    } else {
+        console.log('not small');
+        wrapper.insertBefore(menu, header.nextSibling);
     }
 }
