cbad97f430e5737748ecdd537160d07b5f17837e
Victor Nystad <vnystad@gmail.com>
2014-11-28 16:36:28 +0100

    Update 2014-11-28T15:36:26.675Z

--- script/script.js
+++ script/script.js
@@ -3,30 +3,40 @@ var menu = document.querySelector('nav'), header = document.querySelector('heade
 var hugeMin = window.matchMedia('(min-width: 1280px)');
 var smallMax = window.matchMedia('(max-width: 767px)');
 
-hugeMin.addListener(isHuge);
-isHuge(hugeMin);
+hugeMin.addListener(testMedia);
+testMedia(hugeMin);
 
-smallMax.addListener(isSmall);
-isSmall(smallMax);
+smallMax.addListener(testMedia);
+testMedia(smallMax);
 
-function isHuge(mq) {
+function testMedia(mq) {
     if (hugeMin.matches) {
-        console.log('huge');
         header.appendChild(menu);
-      /* the view port is at least 400 pixels wide */
-    } else {
-        header.parentNode.insertBefore(menu, header.nextSibling);
-        console.log('less than huge');
-      /* the view port is less than 400 pixels wide */
-    }
-}
-
-function isSmall(mq) {
-    if (smallMax.matches) {
-        console.log('small or less');
+    } else if (smallMax.matches) {
         document.body.insertBefore(menu, wrapper.nextSibling);
     } else {
-        console.log('not small');
-        wrapper.insertBefore(menu, header.nextSibling);
+        header.parentNode.insertBefore(menu, header.nextSibling);
     }
 }
+
+// function isHuge(mq) {
+//     if (hugeMin.matches) {
+//         console.log('huge');
+//         header.appendChild(menu);
+//       /* the view port is at least 400 pixels wide */
+//     } else {
+//         header.parentNode.insertBefore(menu, header.nextSibling);
+//         console.log('less than huge');
+//       /* the view port is less than 400 pixels wide */
+//     }
+// }
+//
+// function isSmall(mq) {
+//     if (smallMax.matches) {
+//         console.log('small or less');
+//         document.body.insertBefore(menu, wrapper.nextSibling);
+//     } else {
+//         console.log('not small');
+//         wrapper.insertBefore(menu, header.nextSibling);
+//     }
+// }
